9fbbeab1f894ebc61f8129b11e96c526de6db44f
spertch9a <fortasoussamailyes@gmail.com>
2018-04-21 11:52:37 +0200

    Added a connection to SQL database with adaptaation to node.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ProjetDevWEb
+
+
+
+15/04/2018 ;
+@spertch9a:
+changed colors, changed fonts and reorganized the componets;
--- Scripts/accueil.js
+++ Scripts/accueil.js
@@ -20,3 +20,23 @@ function precedent(){
 	}
 	slider.src=images[num];
 }
+
+
+
+//Partie base de données :
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
