2eea07eefe0581b10d99626496186e8baafa7d82
Fcmam5 <dr.fcmam5@gmail.com>
2018-05-20 06:04:06 +0100

    fix conflict in styles

--- annonce.php
+++ annonce.php
@@ -6,15 +6,11 @@
 	<link rel="stylesheet" type="text/css" href="inscription.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="materialize.min.css"  media="screen,projection"/>
-<<<<<<< HEAD
 	<style media="screen">
 		.form-container{
 			padding: 1%;
 		}
 	</style>
-=======
-
->>>>>>> 12380a37bd1b1eb35aee19ca3eb881823b374f7a
 </head>
 <body>
 	<div class="page">
