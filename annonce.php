1d48718cc02e1d20a3a1de1c5946a62ffa48b362
Fcmam5 <dr.fcmam5@gmail.com>
2018-05-20 04:21:08 +0100

    setup annonce form

--- annonce.php
+++ annonce.php
@@ -6,7 +6,11 @@
 	<link rel="stylesheet" type="text/css" href="inscription.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="materialize.min.css"  media="screen,projection"/>
-
+	<style media="screen">
+		.form-container{
+			padding: 1%;
+		}
+	</style>
 </head>
 <body>
 	<div class="page">
@@ -19,92 +23,53 @@
 			<li> <img id="tel" src="téléchargement.png"> N° local 041 35 26 94 </li>
 		</ul>
 	</nav>
-    <form  method="post" action="traitement.php">
-        <label for="titre" id="titre">
-          <input name="titre"type="text" id="titre" placeholder="Entrez votre titre">
-					<span>titre</span>
-				</label>
-        <label for="delais" id="delais">
-          <input name="delais"type="text" id="delais" placeholder="Entrez votre delais">
-					<span>Delais (jours)</span>
-				</label>
-        <div class="input-field">
-					<label for="Description" id="delais">Description</label>
-					<textarea name="Description" id="Description" class="materialize-textarea" rows="8"></textarea>
-        </div>
 
-				<!-- Last name:  -->
-        <br>
+    <div class="form-container">
+			<h1>Annonce</h1>
+			<form  method="post" action="traitement.php">
+				<div class="input-field">
+					<label for="titre" id="titre">Titre</label>
+						<input name="titre"type="text" id="titre" placeholder="Entrez votre titre">
+				</div>
 
-			 <label for="prenom" id="prenom">Prenom :</label>
-        <input name="prenom" type="text" id="prenom" placeholder="Entrez votre prenom">
-        <br>
-        <!-- Date:   -->
+				<div class="input-field">
+					<label for="delais" id="delais">
+						<span>Delais (jours)</span>
+					</label>
+					<input name="delais"type="text" id="delais" placeholder="Entrez votre delais">
+				</div>
 
-        <label for="username" id="username"> Identifiant :</label>
-        <input name="username"  type="text" id="username" placeholder="Entrez votre identifiant" >
+				<div class="input-field">
+					<label for="budget" id="budget">
+						<span>Budget (Dinars Algerian)</span>
+					</label>
+						<input name="budget"type="text" id="budget" placeholder="Entrez votre budget">
+				</div>
 
-        <br>
-
-
-        <!-- Email:  -->
-
-          <label for="email" id="email">Email :</label>
-          <input name="email" type="email" id="email" placeholder="Entrez votre mail" >
-        <br>
-
-          <label for="password" id="password">Password :</label>
-          <input name="password" type="password" id="password" placeholder="Entrez votre mot de passe">
-        <br>
-
-        <label for="datedenaissance" id="datedenaissance"> Date de naissance :</label>
-        <input type="date" name="datedenaissance" value="" id="datedenaissance" >
-
-        <br>
-
-        Vous êtes : <br>
-        <label for="client">
-					<input type="radio" name="typeofuser" checked value="client" id="client">
-					<span>Client</span>
-				</label>
-        <label for="worker">
-					<input type="radio" name="typeofuser" value="worker" id="worker">
-        	<span>Worker</span>
-				</label>
-        <label for="both">
-					<input type="radio" name="typeofuser" value="both" id="both">
-					<span>Both</span>
-				 </label>
-<div id="professionDiv">
-
-	<label for="proffession" id="proffession">Veuillez saisir vortre proffession</label>
-	<input name="proffession" type="text" id="proffession" placeholder="ex: Plomberie">
-</div>
-
-        <br>
-
-      <button id="submit" class="btn waves-effect waves-light" type="submit" name="action">Submit
-    		<i class="material-icons right">send</i>
-			</button>
-    </form>
+					<div class="file-field input-field">
+						<div class="btn">
+							<span>Image</span>
+							<input type="file">
+						</div>
+						<div class="file-path-wrapper">
+							<input class="file-path validate" type="text">
+						</div>
+					</div>
+					<div class="input-field">
+						<label for="Description" id="description">Description</label>
+						<textarea name="Description" id="Description" class="materialize-textarea" rows="8"></textarea>
+					</div>
+				<button id="submit" class="btn waves-effect waves-light" type="submit" name="action">Submit
+					<i class="material-icons right">send</i>
+				</button>
+			</form>
+    </div>
   </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
  <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
  <script type="text/javascript">
- var checkBoxes = document.getElementsByName('typeofuser');
- var professionDiv = document.getElementById('professionDiv');
- professionDiv.style.display = 'none';
-
- for (var i = 0; i < checkBoxes.length; i++) {
-   checkBoxes[i].addEventListener("click", function(evt) {
-     if(document.getElementById('worker').checked) {
-       professionDiv.style.display = 'block';
-     } else {
-       professionDiv.style.display = 'none';
-     }
-
-   })
- } </script>
+	// TODO
+ </script>
 </body>
 </html>
